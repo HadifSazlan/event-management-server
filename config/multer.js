083296b26d33fc49e49dce8b2bf587ef4aac7554
HadifSazlan <161570497+HadifSazlan@users.noreply.github.com>
2024-07-09 16:56:23 +0800

    UPDATE 2 - Configured Express to serve static files - Implemented multer middleware for file uploads - Integrate storage config, file size limits & type validation

---
+++ config/multer.js
@@ -0,0 +1,35 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const extension = path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix + extension);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: JPEG/JPG/PNG/GIF Files Only");
+  }
+}
+
+export default upload;
